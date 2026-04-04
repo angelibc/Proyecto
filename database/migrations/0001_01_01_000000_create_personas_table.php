27763ead67e07fa2c286e041d6365a77e36889b9
Miller <cabellordz15@gmail.com>
2026-04-04 09:55:45 -0600

    first commit

---
+++ database/migrations/0001_01_01_000000_create_personas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->enum('sexo',['M','F',O]);
+            $table->date('fecha_nacimiento');
+            $table->string('CURP')->unique();
+            $table->string('RFC')->unique();
+            $table->integer('telefono_personal')->unique();
+            $table->integer('celular')->unique();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
---
+++ database/migrations/2026_04_04_043029_create_distribuidoras__table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('distribuidoras', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->unique()->constrained('usuarios')->onDelete('cascade');
+            $table->foreignId('categoria_id')->constrained('categorias');
+            $table->string('estado');
+            $table->decimal('linea_credito', 10, 2)->default(0);
+            $table->integer('puntos')->default(0);
+            $table->decimal('geolocalizacion_lat', 10, 7)->nullable();
+            $table->decimal('geolocalizacion_lng', 10, 7)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('distribuidoras_');
+    }
+};
